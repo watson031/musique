550387e646207c30375db5437156c0ded2372fca
Giselle <gblandin@isi-mtl.com>
2021-01-20 23:35:32 -0500

    css

--- travail-pratique/src/component/playlist-select-component.js
+++ travail-pratique/src/component/playlist-select-component.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PlaylistSelectComponent = ({ id, name, playlists, onClick }) => (
     <div>
-        <label htmlFor={id}>Music</label>
+        <label htmlFor={id} className='labelMusic'>Music</label>
         <select name={name} id={id} onClick={onClick}>
             {playlists.map((option, index) => <option value={option.id} key={index}>{option.title}</option>)}
         </select>
